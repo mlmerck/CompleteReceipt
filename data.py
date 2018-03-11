b8db694bf17d505ab95d21cd98a26aca8415759a
Matthew Merck <36314143+mlmerck@users.noreply.github.com>
2018-03-11 05:08:49 -0400

    Accounts for any new data
    Yes

--- data.py
+++ data.py
@@ -1,14 +1,31 @@
 import os
 import matplotlib.pyplot as plt
+import testdata as expensedata
+
+#takes cost data and finds average costs
+costs = expensedata.costs
+totalcost = 0
+for i in costs:
+    totalcost += i
+percentagecost = []
+for i in costs:
+    percentagecost.append(i/totalcost)
+
 
 # Pie chart
-labels = ['Fast Food', 'Gasoline', 'Utilities', 'Miscellaneous']
-sizes = [15, 30, 45, 10]
+labels = expensedata.costtypes
+count = 0
+labels1 = []
+for i in labels:
+    labels1.append(labels[count].title())
+    count += 1
+
+sizes = percentagecost
 # colors
 colors = ['#ff9999', '#66b3ff', '#99ff99', '#ffcc99']
 
 fig1, ax1 = plt.subplots()
-ax1.pie(sizes, colors=colors, labels=labels, autopct='%1.1f%%', startangle=90)
+ax1.pie(sizes, colors=colors, labels=labels1, autopct='%1.1f%%', startangle=90)
 # draw circle
 centre_circle = plt.Circle((0, 0), 0.70, fc='white')
 fig = plt.gcf()
@@ -21,4 +38,3 @@ plt.tight_layout()
 #Saves file
 os.remove("/Users/Matthew/Documents/Receipt-Bag/piechart1.png")
 fig1.savefig('piechart1.png', facecolor=fig1.get_facecolor(), edgecolor='none')
-
